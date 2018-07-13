a434026976f197cfe84cada0fc79b6935e475406
scooorpion <mhalizadeh@hotmail.com>
2018-07-13 20:01:29 +0430

    Generating No Construction Cells

--- The Happy Neighborhood/Assets/Scripts/BoardGenerator.cs
+++ The Happy Neighborhood/Assets/Scripts/BoardGenerator.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine.UI;
+using System.Collections;
 using UnityEngine;
 using System;
 
-public class BoardGenerator : MonoBehaviour {
+public class BoardGenerator : MonoBehaviour
+{
+
+    public GameObject[] NoConstructionPrefab;
     public GameObject GridLinePrefab;
     public GameObject GridCellPrefab;
     public GameObject BoardBackground;
     public float BoardSizeInPixle = 700f;
     public byte BoardCellsInRow = 7;
     public byte GridLineWidth = 5;
+    public byte NoConstructionCells = 4;
 
     private float gridLinesDistnace;
     private float firstGridPosition;
@@ -22,10 +27,11 @@ public class BoardGenerator : MonoBehaviour {
     public GameObject[,] BoardCellsArray;
 
 
-	void Start ()
+    void Start()
     {
         BoardCellsArray = new GameObject[BoardCellsInRow, BoardCellsInRow];
         GenerateBoard();
+
     }
 
 
@@ -37,10 +43,12 @@ public class BoardGenerator : MonoBehaviour {
         BoardBackground.GetComponent<RectTransform>().sizeDelta = new Vector2(BoardSizeInPixle, BoardSizeInPixle);
 
         generateGridLines();
-
         generateGridCells();
+        GeneratingNoConstCells();
+
     }
 
+    #region generateGridLines()
     void generateGridLines()
     {
         GameObject tempGridRow; // Temp GameObject for generating gridlines
@@ -56,12 +64,12 @@ public class BoardGenerator : MonoBehaviour {
         #endregion
 
         #region Calculating first cell position to generate
-        firstCellPosition = -(BoardSizeInPixle/2);
+        firstCellPosition = -(BoardSizeInPixle / 2);
         #endregion
 
 
         #region Generating Vertical Lines
-        for (int i = 0; i < (BoardCellsInRow-1); i++)
+        for (int i = 0; i < (BoardCellsInRow - 1); i++)
         {
             RectTransform gridRectTransform;
             tempGridRow = Instantiate<GameObject>(GridLinePrefab);          // Generating Vertical Lines
@@ -84,10 +92,11 @@ public class BoardGenerator : MonoBehaviour {
         }
         #endregion
     }
+    #endregion
 
+    #region generateGridCells()
     void generateGridCells()
     {
-        
         for (int i = 0; i < BoardCellsInRow; i++)
         {
             for (int j = 0; j < BoardCellsInRow; j++)
@@ -98,12 +107,58 @@ public class BoardGenerator : MonoBehaviour {
                 cellRectTransform.sizeDelta = new Vector2((gridLinesDistnace - GridLineWidth), (gridLinesDistnace - GridLineWidth));
                 cellSizeWithGridLineOffset = cellRectTransform.rect.width + GridLineWidth;
                 BoardCellsArray[i, j].transform.SetParent(cellsParent, false);
-                cellRectTransform.anchoredPosition = new Vector3(firstCellPosition + (cellSizeWithGridLineOffset / 2) + (j* cellSizeWithGridLineOffset),
+                cellRectTransform.anchoredPosition = new Vector3(firstCellPosition + (cellSizeWithGridLineOffset / 2) + (j * cellSizeWithGridLineOffset),
                                                                   firstCellPosition + (cellSizeWithGridLineOffset / 2) + (i * cellSizeWithGridLineOffset),
                                                                   0);
             }
         }
-    }
 
+    }
+    #endregion
+
+    #region NoRandomRepeatedCell
+    void GeneratingNoConstCells()
+    {
+        int[,] tempNoConstruction = new int[2, NoConstructionCells];
+        for (int i = 0; i < NoConstructionCells; i++)
+        {
+            bool RepeatedCell = false;
+            int randomCellsFirstDemension;
+            int randomCellsSecondtDemension;
+            int randomNoConstPrefabIndex = UnityEngine.Random.Range(0, (NoConstructionPrefab.Length));
+
+            // Checking not to use repeated cell for NoConstructionCell
+            do
+            {
+                RepeatedCell = false;
+                randomCellsFirstDemension = UnityEngine.Random.Range(0, BoardCellsInRow);
+                randomCellsSecondtDemension = UnityEngine.Random.Range(0, BoardCellsInRow);
+                for (int j = 0; j < NoConstructionCells; j++)
+                {
+                    if (randomCellsFirstDemension == tempNoConstruction[0, j])
+                    {
+                        if (randomCellsSecondtDemension == tempNoConstruction[1, j])
+                        {
+                            RepeatedCell = true;
+                            print("Repeated");
+                        }
+                    }
+                }
+            }
+            while (RepeatedCell);
+
+            // Changing Board Cell Image to a NoConstructionCell
+            BoardCellsArray[randomCellsFirstDemension, randomCellsSecondtDemension].GetComponent<Image>().overrideSprite =
+                NoConstructionPrefab[randomNoConstPrefabIndex].GetComponent<Image>().sprite;
+
+            // Making NoConstructionCell to unInteractable Button
+            BoardCellsArray[randomCellsFirstDemension, randomCellsSecondtDemension].GetComponent<Button>().interactable = false;
+
+            tempNoConstruction[0, i] = randomCellsFirstDemension;
+            tempNoConstruction[1, i] = randomCellsSecondtDemension;
+
+        }
+    }
+    #endregion
 
 }
